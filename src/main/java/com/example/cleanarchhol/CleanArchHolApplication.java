fb9bd54b03dba0bfb91324a326a0472a98687489
Kangmin Park <kangmin.park@navercorp.com>
2022-01-23 21:34:30 +0900

    Init Commit

---
+++ src/main/java/com/example/cleanarchhol/CleanArchHolApplication.java
@@ -0,0 +1,13 @@
+package com.example.cleanarchhol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchHolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArchHolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cleanarchhol/CleanArchHolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanarchhol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchHolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
