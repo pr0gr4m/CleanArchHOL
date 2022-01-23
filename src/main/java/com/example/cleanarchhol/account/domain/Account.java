007a3ee4d75973ec4576f47e764cae41288ae784
Kangmin Park <kangmin.park@navercorp.com>
2022-01-23 21:59:42 +0900

    Add account domain

---
+++ src/main/java/com/example/cleanarchhol/account/domain/Account.java
@@ -0,0 +1,52 @@
+package com.example.cleanarchhol.account.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Value;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Account {
+    @Getter
+    private final AccountId id;
+
+    @Getter
+    private final Money baselineBalance;
+
+    @Getter
+    private final ActivityWindow activityWindow;
+
+    public Money calculateBalance() {
+        return Money.add(
+                this.baselineBalance,
+                this.activityWindow.calculateBalance(this.id)
+        );
+    }
+
+    public boolean withdraw(Money money, AccountId targetAccountId) {
+        if (!mayWithdraw(money))
+            return false;
+        this.activityWindow.addActivity(new Activity(
+                this.id,
+                this.id,
+                targetAccountId,
+                LocalDateTime.now(),
+                money
+        ));
+        return true;
+    }
+
+    private boolean mayWithdraw(Money money) {
+        return Money.add(
+                this.calculateBalance(),
+                money.negate())
+                .isPositiveOrZero();
+    }
+
+    @Value
+    public static class AccountId {
+        private Long value;
+    }
+}
